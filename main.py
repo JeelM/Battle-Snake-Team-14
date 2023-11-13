88b29a06e72d2058979fef2b5536c061a07549ea
Danielle Wahrhaftig <daniellewahrhaftig@gmail.com>
2023-11-13 15:43:36 -0500

    added changes to avoid boundaries

--- main.py
+++ main.py
@@ -63,8 +63,17 @@ def move(game_state: typing.Dict) -> typing.Dict:
         is_move_safe["up"] = False
 
     # TODO: Step 1 - Prevent your Battlesnake from moving out of bounds
-    # board_width = game_state['board']['width']
-    # board_height = game_state['board']['height']
+    board_width = game_state['board']['width']
+    board_height = game_state['board']['height']
+
+    if my_head["x"] == board_width - 1:
+        is_move_safe["right"] = False
+    elif my_head["x"] == 0:
+        is_move_safe["left"] = False
+    if my_head["y"] == board_height - 1:
+        is_move_safe["up"] = False
+    elif my_head["y"] == 0:
+        is_move_safe["down"] = False
 
     # TODO: Step 2 - Prevent your Battlesnake from colliding with itself
     # my_body = game_state['you']['body']
